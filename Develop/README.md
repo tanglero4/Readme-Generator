0e20d548055e269dd597cc6b480bcc50aac3e0db
tanglero4 <tiffanyanglero94@hotmail.com>
2023-02-01 20:07:01 -0500

    Removed double file

---
+++ Develop/README.md
@@ -0,0 +1,30 @@
+# rEADME
+ ## License
+![alt](https://img.shields.io/badge/License--blue)
+          
+## Table of Content
+- [Project Description](#Description)
+- [Usage](#Usage)
+- [Contribution](#Contribution)
+- [Installation](#Installation)
+- [Tests](#Tests)
+- [Questions](#Questions)
+- [Email](#Email)
+- [Github](#Github)
+## Description
+HJSD
+## Usage
+MAKING REad me
+## Contributing
+you me I
+## Installation
+YES
+## Tests
+dihysdiofsdljmksdf
+## Questions 
+no
+Please contact me using the following links:
+## Email
+[Email: edwsfhju](mailto:edwsfhju)
+## Github
+[GitHub](https://github.com/ouihedsf)    
---
+++ Develop/index.js
@@ -0,0 +1,73 @@
+// TODO: Include packages needed for this application
+const inquirer = require("inquirer")
+const generateMarkdown = require("./utils/generateMarkdown")
+const fs = require("fs")
+// TODO: Create an array of questions for user input
+let questions = [
+    {
+    type: "input",
+    message: "Project title?",
+    name: "title",
+   },
+   {
+    type: "input",
+    message: "Describe the project?",
+    name: "description",
+   },
+   {
+    type: "input",
+    message: "Table of Contents.",
+    name: "table-of-contents",
+   },
+   {
+    type: "input",
+    message: "Provide installation instructions.",
+    name: "installation",
+   },
+   {
+    type: "input",
+    message: "What is the project used for?",
+    name: "usage",
+   },
+   {
+    type: "checkbox",
+    message: "Do you want to add a license?",
+    choices: ["MIT", "Apache 2", "Boost Software License 1.0"],
+    name: "license",
+   },
+   {
+    type: "input",
+    message: "Who helped contribute to this project?",
+    name: "contributors",
+   },
+   {
+    type: "input",
+    message: "Do you have any test instructions?",
+    name: "testInstructions",
+   },
+   {
+    type: "input",
+    message: "Have questions?",
+    name: "questions",
+   },
+      {
+    type: "input",
+    message: "What is your Github url?",
+    name: "Github",
+   },
+   {
+    type: "input",
+    message: "What is your email address?",
+    name: "email",
+   },
+
+];
+
+
+// Initialize app function
+ function init() {
+
+};
+
+// Call to initialize app
+init();
